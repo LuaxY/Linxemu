b194310a420c5506e3490e8830fa8b99237c58d5
Sorrow- <yann.guineau@gmail.com>
2013-05-13 22:53:24 +0200

    Add title display

--- Linxemu.cpp
+++ Linxemu.cpp
@@ -1,12 +1,25 @@
+#include <iostream>
+
 #include "config/AuthConfig.h"
 #include "network/managers/AuthServer.h"
 
+using namespace std;
+
+void displayTitle();
+
 int main()
 {
+    displayTitle();
+
     AuthConfig authConfig;
     AuthServer authServer;
 
     authServer.start(authConfig.getPort(), authConfig.getMaxClients());
 
     return 0;
+}
+
+void displayTitle()
+{
+    cout << "Linxemu (ALPHA) par Sorrow." << endl << endl;
 }
