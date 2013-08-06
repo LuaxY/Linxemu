27b78f5ebd0c4fd09cdfe17f15a0a616c40325d8
Sorrow- <yann.guineau@gmail.com>
2013-08-06 18:53:43 +0200

    Add position logger for debug

--- utils/utils.h
+++ utils/utils.h
@@ -1,8 +1,11 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include "hexdump.h"
 #include "Logger.h"
 #include "MessageReader.h"
 #include "MessageWriter.h"
+
+#define positionLog() { cout << (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__) << ":" << __LINE__ << ":" << __FUNCTION__ << "()" << endl; }
 
 #endif // UTILS_H
