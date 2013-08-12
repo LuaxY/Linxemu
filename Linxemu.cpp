1a446db4ed168fba04b2c89bce850ee89de38013
Sorrow- <yann.guineau@gmail.com>
2013-08-12 18:39:37 +0200

    Fix bad include

--- Linxemu.cpp
+++ Linxemu.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
 #include "config/AuthConfig.h"
-#include "worker/managers/Worker.h"
-#include "network/managers/AuthServer.h"
+#include "worker/Worker.h"
+#include "network/AuthServer.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Linxemu (DEV) by Sorrow (" << __DATE__ << " " << __TIME__ << ")" << endl << endl;
+
+    cout << sizeof(unsigned long) << endl;
 
     AuthConfig authConfig;
     AuthServer authServer;
